63bb1a66f6d570df90e9acf705f36dd6967e1aa8
Elisha Andrew <elishaa@phreetech.com>
2025-01-29 09:50:37 +0100

    service and training page

--- src/route.ts
+++ src/route.ts
@@ -3,11 +3,13 @@ import expressConfig from "./app";
 
 import IndexController from "./controllers";
 import TrainingController from "./controllers/training";
+import ServicesController from "./controllers/services";
 
 // Route groups
 import index_route from "./routes/index";
 import training from "./routes/training";
 import contact from "./routes/contact";
+import services from "./routes/services";
 
 const AppRoutes = () => {
     const app = expressConfig();
@@ -16,6 +18,7 @@ const AppRoutes = () => {
     app.use('/', index_route);
     app.use('/', training);
     app.use('/', contact);
+    app.use('/', services);
 
     // No matching route
     app.use((req: Request, res: Response, next: NextFunction) => {
